9388fddd84a3f918de39238af8d016beb294e7cc
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2024-03-25 17:22:27 +0700

    FIX android updater name

--- hub/updater/updater.go
+++ hub/updater/updater.go
@@ -138,7 +138,7 @@ func prepare(exePath string) (err error) {
 	if runtime.GOOS == "windows" {
 		updateExeName = "mihomo" + "-" + runtime.GOOS + "-" + runtime.GOARCH + amd64Compatible + ".exe"
 	} else if runtime.GOOS == "android" && runtime.GOARCH == "arm64" {
-		updateExeName = "mihomo-android-arm64-v8"
+		updateExeName = "mihomo-android-arm64"
 	} else {
 		updateExeName = "mihomo" + "-" + runtime.GOOS + "-" + runtime.GOARCH + amd64Compatible
 	}
